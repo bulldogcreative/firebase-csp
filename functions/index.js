4ffd041fd03c7698142d820f875524ff14ff23fb
Levi Durfee <levi.durfee@gmail.com>
2019-05-08 08:42:16 -0400

    import URL

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,16 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const url = require('url');
 admin.initializeApp(functions.config().firebase);
 
 exports.cspReport = functions.https.onRequest((request, response) => {
 
     const report = JSON.parse(request.body.toString('utf8'));
-    const url = new URL(report.document-uri);
+    const myurl = new URL(report.document-uri);
 
     report.created_at = Date.now();
 
-    const writeResult = admin.firestore().collection(url.hostname).add(report);
+    const writeResult = admin.firestore().collection(myurl.hostname).add(report);
     writeResult.then((ref) => {
         console.log(`Message with ID: ${ref.id} added.`);
         // We get a CORBS Error when returning a response.
